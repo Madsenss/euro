e9d74fb511cd39f144d7cff5d130ab46963e1ed2
madsens <narcisselove@naver.com>
2023-11-07 09:57:18 +0900

    detail 상단 수정완료, 후기 등 하단 수정해야함

--- client/src/components/detail/detailtop.js
+++ client/src/components/detail/detailtop.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { MdAdd, MdHome, MdOutlineKeyboardArrowRight, MdRemove } from "react-icons/md";
+import { MdAdd, MdClose, MdFavorite, MdHome, MdKeyboardArrowDown, MdOutlineKeyboardArrowRight, MdRemove } from "react-icons/md";
 import styled from "styled-components";
 
 const DetailTopBox = styled.div`
@@ -7,7 +7,6 @@ const DetailTopBox = styled.div`
   height: fit-content;
   display: flex;
   flex-direction: column;
-  border: 1px solid red;
 `
 const RootBox = styled.div`
   padding: 15px 0px 15px 0px;
@@ -37,11 +36,11 @@ const ImgBox = styled.div`
   flex-direction: row;
   justify-content: space-between;
   .product-img {
-    width: 450px;
+    width: 500px;
+    height: 500px;
   }
   .itembox {
-    border: 1px solid red;
-    width: 600px;
+    width: 500px;
     height: fit-content;
     .item {
       padding: 20px 0px 20px 0px;
@@ -76,13 +75,7 @@ const ImgBox = styled.div`
         color: var(--color);
         font-weight: bold;
       }
-      .total {
-        font-size: 18px;
-        font-weight: bold;
-        margin-left: auto;
-      }
     }
-    
     .infobox {
       display: flex;
       flex-direction: column;
@@ -98,6 +91,25 @@ const ImgBox = styled.div`
         }
       }
     }
+    .total {
+      width: 100%;
+      height: fit-content;
+      margin: 50px 0px 30px 0px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: end;
+      .total-title {
+        font-size: 18px;
+        color: #666;
+      }
+      .total-price {
+        font-weight: bold;
+        margin-left: 10px;
+        font-size: 26px;
+        color: #444;
+      }
+    }
   }
 `
 const CountBox = styled.div`
@@ -107,6 +119,7 @@ const CountBox = styled.div`
   align-items: center;
   justify-content: center;
   box-shadow: 0px 0px 2px 0.5px rgb(0, 0, 0, 0.2);
+  margin-right: 15px;
   .box {
     width: 30px;
     height: 30px;
@@ -130,20 +143,206 @@ const CountBox = styled.div`
   }
 `
 
+const OptionBox = styled.div`
+  width: 480px;
+  height: 40px;
+  position: relative;
+`
+const OptionDropdownBox = styled.div`
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0px 0px 2px 0.5px rgb(0, 0, 0, 0.2);
+  padding: 0px 10px 0px 10px;
+  font-size: 14px;
+  .down {
+    font-size: 16px;
+    transition: all 0.2s;
+    &.active {
+      transform: rotate(180deg);
+    }
+  }
+`
+const OptionMenu = styled.div`
+  z-index: 1001;
+  background-color: #fff;
+  position: absolute;
+  right: 0;
+  top: 40px;
+  width: 100%;
+  height: 500px;
+  box-shadow: 0px 0px 2px 0.5px rgb(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  transition: all 0.1s;
+  &.show {
+    height: 160px;
+    visibility: visible;
+    opacity: 1;
+  }
+  &.hide {
+    height: 0px;
+    visibility: hidden;
+    opacity: 0;
+  }
+  .option-item {
+    cursor: pointer;
+    width: 100%;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    transition: all 0.2s;
+    font-size: 14px;
+    padding-left: 10px;
+    &:hover {
+      background-color: #eee;
+    }
+    .icon {
+      color: rgb(255, 165, 0);
+    }
+    .ot {
+      max-width: 290px;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+    .op {
+      margin-left: 10px;
+      font-weight: bold;
+    }
+  }
+`
+
+const OptionCountBox = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+`
+const OptionItem = styled.div`
+  position: relative;
+  width: 100%;
+  height: fit-content;
+  padding: 10px 0px 10px 0px;
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  .option-price {
+    margin-left: auto;
+  }
+  .option-title {
+    max-width: 250px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+  .delete {
+    cursor: pointer;
+    border-radius: 50%;
+    padding: 2px;
+    font-size: 14px;
+    background-color: var(--color);
+    color: #fff;
+    margin-left: 10px;
+  }
+`
+
+const ButtonBox = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+const Button = styled.div`
+  cursor: pointer;
+  width: 160px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  border-radius: 3px;
+  background-color: var(--color);
+  border: 1.5px solid transparent;
+  box-shadow: 0px 0px 2px 1px rgb(0, 0, 0, 0.1);
+  transition: all 0.2s;
+`
+const CartButton = styled(Button)`
+  background-color: #fff;
+  border: 1.5px solid var(--color);
+  color: var(--color);
+`
+const WishButton = styled(Button)`
+  margin-left: auto;
+  margin-right: 0;
+  width: 40px;
+  background-color: #aaa;
+  color: #eee;
+  &:hover {
+    color: var(--color);
+    background-color: #fff;
+  }
+`
+
 const DetailTop = () => {
 
-  const [count, setCount] = useState(1);
+  const [option, setOption] = useState('옵션을 선택하세요');
+  const [optionActive ,setOptionActive] = useState(false);
+  const [totalOption, setTotalOption] = useState([]);
 
-  const handleAdd = () => {
-    setCount(count + 1);
+  const increaseOptionCount = (index) => {
+    const updatedOptions = [...totalOption];
+    updatedOptions[index].count += 1;
+    setTotalOption(updatedOptions);
   };
-  
-  const handleRemove = () => {
-    if (count === 1) {
-      return null;
-    } else {
-      setCount(count - 1);
+
+  const decreaseOptionCount = (index) => {
+    const updatedOptions = [...totalOption];
+    if (updatedOptions[index].count > 1) {
+      updatedOptions[index].count -= 1;
+      setTotalOption(updatedOptions);
     }
+  };
+
+  const initData = [
+    {name: 'L Size', price: 10000, count: 1},
+    {name: 'XL Size', price: 20000, count: 1},
+    {name: 'XXL Size', price: 30000, count: 1},
+  ];
+
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat('ko-KR').format(price);
+  };
+
+  const AddOption = (name, price, count) => {
+    const isOptionExists = totalOption.some((option) => option.name === name && option.price === price);
+  
+    if (!isOptionExists) {
+      const newOption = { name, price, count };
+      setTotalOption([...totalOption, newOption]);
+    }
+  };
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    totalOption.forEach((option) => {
+      totalPrice += option.price * option.count;
+    });
+    return totalPrice;
+  };
+
+  const removeOption = (index) => {
+    const updatedOptions = [...totalOption];
+    updatedOptions.splice(index, 1);
+    setTotalOption(updatedOptions);
   };
 
   return (
@@ -166,7 +365,7 @@ const DetailTop = () => {
           </div>
           <div className="item">
             <span className="item-title">판매가</span>
-            <span className="price">99,999</span>
+            <span className="price">99,999원</span>
           </div>
           <div className="item">
             <span className="item-title">배송비</span>
@@ -202,19 +401,60 @@ const DetailTop = () => {
             </div>
           </div>
           <div className="item">
-            <span className="item-title">상품수량</span>
-            <CountBox>
-              <div className="box cp" onClick={()=>{handleRemove();}}>
-                <MdRemove className="icon"/>
-              </div>
-              <div className="box">
-                <span className="count">{count}</span>
-              </div>
-              <div className="box cp" onClick={()=>{handleAdd();}}>
-                <MdAdd className="icon"/>
-              </div>
-            </CountBox>
+            <span className="item-title">상품옵션</span>
+            <OptionBox>
+              <OptionDropdownBox onClick={() => { setOptionActive(!optionActive) }}>
+                <span>{option}</span>
+                <MdKeyboardArrowDown className={'down ' + `${optionActive ? 'active' : ''}`} />
+              </OptionDropdownBox>
+              <OptionMenu className={optionActive ? 'show' : 'hide'}>
+                <div className="option-item" onClick={() => { setOption('옵션을 선택하세요'); setOptionActive(false) }}>옵션을 선택하세요</div>
+                {
+                  initData.map((item, i)=>{
+                    const formattedPrice = formatPrice(item.price);
+                    return (
+                      <div className="option-item" key={i} onClick={() => { setOption('옵션을 선택하세요'); setOptionActive(false); AddOption(item.name, item.price, item.count); }}>
+                        <span className="ot">{item.name}</span>
+                        <span className="op">{formattedPrice}원</span>
+                      </div>
+                    )
+                  })
+                }
+              </OptionMenu>
+            </OptionBox>
           </div>
+          <OptionCountBox>
+            {
+              totalOption&&totalOption.map((item, i)=>{
+                return (
+                  <OptionItem key={i}>
+                    <CountBox>
+                      <div className="box cp" onClick={()=>{decreaseOptionCount(i)}}>
+                        <MdRemove className="icon"/>
+                      </div>
+                      <div className="box">
+                        <span className="count">{item.count}</span>
+                      </div>
+                      <div className="box cp" onClick={()=>{increaseOptionCount(i);}}>
+                        <MdAdd className="icon"/>
+                      </div>
+                    </CountBox>
+                    <span className="option-title">{item.name}</span>
+                    <span className="option-price">{formatPrice(parseInt(item.price) * parseInt(item.count))}원</span>
+                    <MdClose className="delete" onClick={() => {removeOption(i);}} />
+                  </OptionItem>
+                )
+              })
+            }
+          </OptionCountBox>
+          <div className="total">
+            <span className="total-title">총 상품금액<span className="total-price">{formatPrice(calculateTotalPrice())}</span>원</span>           
+          </div>
+          <ButtonBox>
+            <CartButton>장바구니</CartButton>
+            <Button>구매하기</Button>
+            <WishButton><MdFavorite/></WishButton>
+          </ButtonBox>
         </div>
       </ImgBox>
     </DetailTopBox>
